20ad4fae1b2f6467b6d333bc996eecb4f38f112f
jubianchi <contact@jubianchi.fr>
2013-01-12 01:11:55 +0100

    Fix logger plugin

--- src/jubianchi/Jubot/Philip/Plugin/Logger.php
+++ src/jubianchi/Jubot/Philip/Plugin/Logger.php
@@ -14,51 +14,17 @@ class Logger extends AbstractPlugin
 
     public function init()
     {
-        $config = $this->config;
-
         $this->bot->onChannel('/.*/', $this->getLoggerCallback());
 
         $this->bot->onChannel(
             '/^!log (?:(?P<since>[a-zA-Z0-9][a-zA-Z0-9 +\-]*)|)(?:\.\.(?P<until>[a-zA-Z0-9][a-zA-Z0-9 +\-]*?))?(?: -(?:f |-from=)(?P<from>[a-zA-Z][a-zA-Z0-9\-\[\]\\`\^{}]*)|$)/',
-            function(Event $event) use ($config) {
-                $matches = $event->getMatches();
-                $file = $config['path'] . DIRECTORY_SEPARATOR . trim($event->getRequest()->getSource(), '#');
-
-                try {
-                    $since = new \DateTime(isset($matches['since']) ? $matches['since'] : '1 hour ago');
-                    $until = new \DateTime(isset($matches['until']) ? $matches['until'] : 'now');
-                    $from = isset($matches['from']) ? trim($matches['from']) : null;
-
-                    if (file_exists($file)) {
-                        foreach (file($file) as $line) {
-                            $parts = explode("\t", $line);
-
-                            try {
-                                $date = new \DateTime($parts[0]);
-
-                                if(
-                                    $date >= $since && $date < $until &&
-                                    (null === $from || $event->getRequest()->getSendingUser() === $from)
-                                ) {
-                                    $event->addResponse(
-                                        Response::msg(
-                                            $event->getRequest()->getSendingUser(),
-                                            sprintf(
-                                                '[%s]  <%s>  %s',
-                                                $date->format('d/m/Y H:i:s'),
-                                                trim($parts[1]),
-                                                trim($parts[2])
-                                            )
-                                        )
-                                    );
-                                }
-                            } catch (\Exception $exception) {}
-                        }
-                    }
-                } catch (\Exception $exception) {}
-
-            }
+			$this->getLogCallback()
         );
+
+		$this->bot->onPrivateMessage(
+			'/^!log (?P<channel>([#&][^\x07\x2C\s]{0,200})) (?:(?P<since>[a-zA-Z0-9][a-zA-Z0-9 +\-]*)|)(?:\.\.(?P<until>[a-zA-Z0-9][a-zA-Z0-9 +\-]*?))?(?: -(?:f |-from=)(?P<from>[a-zA-Z][a-zA-Z0-9\-\[\]\\`\^{}]*)|$)/',
+			$this->getLogCallback()
+		);
     }
 
     protected function getLoggerCallback()
@@ -69,6 +35,15 @@ class Logger extends AbstractPlugin
             $self->log($event);
         };
     }
+
+	protected function getLogCallback()
+	{
+		$self = $this;
+
+		return function(Event $event) use ($self) {
+			$self->display($event);
+		};
+	}
 
     public function log(Event $event)
     {
@@ -87,6 +62,46 @@ class Logger extends AbstractPlugin
         }
     }
 
+	public function display(Event $event)
+	{
+		$matches = $event->getMatches();
+		$channel = isset($matches['channel']) ? $matches['channel'] : $event->getRequest()->getSource();
+		$file = $this->config['path'] . DIRECTORY_SEPARATOR . trim($channel, '#');
+
+		try {
+			$since = new \DateTime(isset($matches['since']) ? $matches['since'] : '1 hour ago');
+			$until = new \DateTime(isset($matches['until']) ? $matches['until'] : 'now');
+			$from = isset($matches['from']) ? trim($matches['from']) : null;
+
+			if (file_exists($file)) {
+				foreach (file($file) as $line) {
+					$parts = explode("\t", $line);
+
+					try {
+						$date = new \DateTime($parts[0]);
+
+						if(
+							$date >= $since && $date < $until &&
+							(null === $from || $event->getRequest()->getSendingUser() === $from)
+						) {
+							$event->addResponse(
+								Response::msg(
+									$event->getRequest()->getSendingUser(),
+									sprintf(
+										'[%s]  <%s>  %s',
+										$date->format('d/m/Y H:i:s'),
+										trim($parts[1]),
+										trim($parts[2])
+									)
+								)
+							);
+						}
+					} catch (\Exception $exception) {}
+				}
+			}
+		} catch (\Exception $exception) {}
+	}
+
     public function boot(array $config = array())
     {
         parent::boot($config);
@@ -100,6 +115,7 @@ class Logger extends AbstractPlugin
     {
         $event->addResponse(Response::msg($event->getRequest()->getSource(), '* Logger'));
         $event->addResponse(Response::msg($event->getRequest()->getSource(), '*    !log [[<since>=1 hour ago]..[<until>=now]] [-f|--from=<from>]'));
+        $event->addResponse(Response::msg($event->getRequest()->getSource(), '*    [priv] !log <#channel> [[<since>=1 hour ago]..[<until>=now]] [-f|--from=<from>]'));
         $event->addResponse(Response::msg($event->getRequest()->getSource(), '*'));
     }
 }
